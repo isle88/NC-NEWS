dc1c819f21a69f084daced4eb7a7c1ba8e6a1147
isle88 <hyunjungkim2014@gmail.com>
2022-03-10 18:31:14 +0000

    add deleteComment

--- src/utils/api.js
+++ src/utils/api.js
@@ -16,6 +16,12 @@ export const fetchArticles = () => {
   });
 };
 
+export const fetchArticleById = (article_id) => {
+  return myApi.get(`articles/${article_id}`).then(({ data }) => {
+    return data.article;
+  })
+}
+
 export const fetchComments = (article_id) => {
   return myApi.get(`/articles/${article_id}/comments`).then(({ data }) => {
     return data.comments;
@@ -36,5 +42,10 @@ export const postComment = (article_id, comment) => {
   .then(({ data }) => {
     return data
   })
+}
+
+export const deleteComment = (comment_id) => {
+  return myApi
+  .delete(`/comments/${comment_id}`)
 }
 
